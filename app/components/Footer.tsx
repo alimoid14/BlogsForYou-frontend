f0155bb009fcbba1ef4388e9972662d83c99fd71
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2024-01-30 12:03:17 +0530

    Add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex bg-[#CCC8AA] dark:bg-slate-800 dark:text-white flex-col items-center sm:flex-row sm:justify-evenly text-xl font-semibold">
+      <div className="py-4 w-96 sm:w-auto">
+        <h2>
+          Developed by: <br />
+          Syed Ali Moid Rizvi
+        </h2>
+        <br />
+        <p>
+          View my{" "}
+          <a href="https://www.linkedin.com/in/syed-ali-moid-rizvi-022355226">
+            LinkedIn here!
+          </a>
+        </p>
+      </div>
+
+      <div className="py-4 w-96 sm:w-auto">
+        <h2>
+          Contact me:
+          <br />
+          Email: <br />
+          syedalimoidrizvi@gmail.com
+        </h2>
+        <p>
+          &#169; {new Date().getFullYear()} <br /> All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+}
