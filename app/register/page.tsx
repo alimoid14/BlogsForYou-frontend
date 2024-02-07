39ab4a3953ef83723583b5846506ece73eae99ec
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2024-02-07 19:52:49 +0530

    Refine register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import ParticlesBg from "../components/ParticlesBg";
+import getUser from "../lib/getUser";
 
 export default function RegistrationPage() {
   const [email, setEmail] = useState("");
@@ -10,6 +11,21 @@ export default function RegistrationPage() {
   const [passCheck, setPassCheck] = useState("");
   const [usernameCheck, setCheckData] = useState("");
   const [emailCheck, setEmailCheckData] = useState("");
+  const [uName, setUname] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userData = await getUser();
+      if (userData !== "") {
+        setUname(userData.username);
+        setLoggedIn(true);
+      }
+      //console.log(uName);
+    };
+
+    fetchData();
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -90,103 +106,133 @@ export default function RegistrationPage() {
   return (
     <main className="text-black dark:text-white flex items-center justify-center main-height">
       <ParticlesBg />
-      <form
-        id="registrationForm"
-        className="flex flex-col items-center border-4 border-black rounded-xl bg-[#CCC8AA] dark:bg-slate-800 bg-opacity-40 p-8 text-xl"
-      >
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          placeholder="Email"
-          required
-          className="text-black"
-          onChange={(e) => {
-            setEmail(e.currentTarget.value);
-          }}
-        />
-        <div className="text-red-500 font-bold font-mono">{emailCheck}</div>
-
-        <label className="mt-4" htmlFor="username">
-          Username:
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          placeholder="Username"
-          required
-          className="text-black"
-          maxLength={10}
-          onChange={(e) => {
-            const input = e.currentTarget.value;
-
-            // Validate input using a regular expression
-            const validInput = input.replace(/[^a-zA-Z0-9]/g, ""); // Remove non-alphanumeric characters
-
-            // Update the username state with the validated input
-            setUserName(validInput.slice(0, 10));
-          }}
-        />
-        <div className="text-red-500 font-bold font-mono">{usernameCheck}</div>
-
-        <label className="mt-4" htmlFor="password">
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          placeholder="Password"
-          required
-          className="text-black"
-          onChange={(e) => {
-            setPass(e.currentTarget.value);
-          }}
-        />
-
-        <label className="mt-4" htmlFor="passCheck">
-          Re-Enter Password:
-        </label>
-        <input
-          type="password"
-          id="passCheck"
-          name="passCheck"
-          value={passCheck}
-          placeholder="Password"
-          required
-          className="text-black"
-          onChange={(e) => {
-            setPassCheck(e.currentTarget.value);
-          }}
-        />
-
-        {emailCheck === "" && usernameCheck === "" ? (
-          <button
-            className="mt-4 font-extrabold text-blue-700"
-            onClick={handleSubmit}
-          >
-            Register
-          </button>
-        ) : (
-          <></>
-        )}
-
-        <br />
-        <div>
-          <p>Already have an account?</p>
-          <p>
-            <span className="text-orange-500 font-bold">
-              <a href="/login">Login</a>
-            </span>{" "}
-            here
+      {loggedIn ? (
+        <div className="mt-8 text-xl text-black dark:text-white">
+          <p className="text-4xl">
+            Logged in as <span className="text-[#F4BF96]">{uName}</span>
           </p>
+          <br />
+          <div className="opacity-70">
+            <button
+              className="text-red-600 font-bold"
+              onClick={async () => {
+                await Axios.post(
+                  "https://blogsserver.onrender.com/logout",
+                  {},
+                  {
+                    withCredentials: true,
+                  }
+                );
+                setUname("");
+                setLoggedIn(false);
+              }}
+            >
+              Logout
+            </button>
+            <span> first to register a new account</span>
+          </div>
         </div>
-      </form>
+      ) : (
+        <form
+          id="registrationForm"
+          className="flex flex-col items-center border-4 border-black rounded-xl bg-[#CCC8AA] dark:bg-slate-800 bg-opacity-40 p-8 text-xl"
+        >
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            required
+            className="text-black"
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+            }}
+          />
+          <div className="text-red-500 font-bold font-mono">{emailCheck}</div>
+
+          <label className="mt-4" htmlFor="username">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            placeholder="Username"
+            required
+            className="text-black"
+            maxLength={10}
+            onChange={(e) => {
+              const input = e.currentTarget.value;
+
+              // Validate input using a regular expression
+              const validInput = input.replace(/[^a-zA-Z0-9]/g, ""); // Remove non-alphanumeric characters
+
+              // Update the username state with the validated input
+              setUserName(validInput.slice(0, 10));
+            }}
+          />
+          <div className="text-red-500 font-bold font-mono">
+            {usernameCheck}
+          </div>
+
+          <label className="mt-4" htmlFor="password">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            required
+            className="text-black"
+            onChange={(e) => {
+              setPass(e.currentTarget.value);
+            }}
+          />
+
+          <label className="mt-4" htmlFor="passCheck">
+            Re-Enter Password:
+          </label>
+          <input
+            type="password"
+            id="passCheck"
+            name="passCheck"
+            value={passCheck}
+            placeholder="Password"
+            required
+            className="text-black"
+            onChange={(e) => {
+              setPassCheck(e.currentTarget.value);
+            }}
+          />
+
+          {emailCheck === "" && usernameCheck === "" ? (
+            <button
+              className="mt-4 font-extrabold text-blue-700"
+              onClick={handleSubmit}
+            >
+              Register
+            </button>
+          ) : (
+            <></>
+          )}
+
+          <br />
+          <div>
+            <p>Already have an account?</p>
+            <p>
+              <span className="text-orange-500 font-bold">
+                <a href="/login">Login</a>
+              </span>{" "}
+              here
+            </p>
+          </div>
+        </form>
+      )}
     </main>
   );
 }
