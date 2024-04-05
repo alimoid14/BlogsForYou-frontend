0785780db97c63c5cbc69dba1a13098fcbc15cf8
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2024-04-05 10:33:16 +0530

    Modify footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,32 +1,41 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa6";
+import { CiTwitter } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="flex bg-[#CCC8AA] dark:bg-slate-800 dark:text-white flex-col items-center sm:flex-row sm:justify-evenly text-xl font-semibold w-screen">
-      <div className="py-4 w-96 sm:w-auto">
+    <footer className="flex bg-[#CCC8AA] dark:bg-slate-800 dark:text-white flex-row flex-wrap justify-evenly text-lg sm:text-xl font-semibold w-screen">
+      <div className="py-4">
         <h2>
           Developed by: <br />
           Syed Ali Moid Rizvi
         </h2>
         <br />
-        <p>
-          View my{" "}
-          <a href="https://www.linkedin.com/in/syed-ali-moid-rizvi-022355226">
-            LinkedIn here!
-          </a>
-        </p>
       </div>
 
-      <div className="py-4 w-96 sm:w-auto">
-        <h2>
-          Contact me:
-          <br />
-          Email: <br />
-          syedalimoidrizvi@gmail.com
-        </h2>
-        <p>
-          &#169; {new Date().getFullYear()} <br /> All rights reserved
-        </p>
+      <div className="py-4">
+        <div className="flex flex-row flex-wrap">
+          <span className=" pr-2">
+            <a href="https://www.linkedin.com/in/syed-ali-moid-rizvi-022355226">
+              <FaLinkedin />
+            </a>
+          </span>
+          <span className="pr-2">
+            <a href="https://twitter.com/alimoidrizvi">
+              <CiTwitter />
+            </a>
+          </span>
+          <span className="pr-2">
+            <a href="https://github.com/alimoid14">
+              <FaGithub />
+            </a>
+          </span>
+        </div>
+
+        <span>
+          &#169; {new Date().getFullYear()} <br />
+        </span>
       </div>
     </footer>
   );
