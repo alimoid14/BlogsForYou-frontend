778342a03fa5bd6218183b2fa8be318fc109af2d
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2024-04-14 13:23:59 +0530

    Display if username or email already exists during registration

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -240,7 +240,9 @@ export default function RegistrationPage() {
               </button>
             )
           ) : (
-            <></>
+            <div className="mt-4 font-extrabold text-red-600 ">
+              Username or email already exists
+            </div>
           )}
 
           <br />
